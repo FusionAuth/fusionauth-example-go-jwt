5f28613ded489e3d9ad8d93081720de8d5852d06
Dan Moore <github@mooreds.com>
2021-02-09 14:06:51 -0700

    First commit.

---
+++ hmac.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"time"
+)
+
+func main() {
+
+	var mySigningKey = []byte("hello gophers!!!")
+
+	// user api
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["aud"] = "238d4793-70de-4183-9707-48ed8ecd19d9"
+	claims["sub"] = "19016b73-3ffa-4b26-80d8-aa9287738677"
+	claims["iss"] = "fusionauth.io"
+	claims["exp"] = time.Now().Add(time.Minute * 5).Unix()
+	claims["name"] = "Dan Moore"
+	var roles [1]string
+	roles[0] = "RETRIEVE_TODOS"
+	claims["roles"] = roles
+
+	tokenString, err := token.SignedString(mySigningKey)
+
+	if err != nil {
+		fmt.Errorf("Something Went Wrong: %s", err.Error())
+	}
+
+	fmt.Println(string(tokenString))
+
+	// todo api
+	decodedToken, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf(("Invalid Signing Method"))
+		}
+
+		aud := ""
+		checkAudience := token.Claims.(jwt.MapClaims).VerifyAudience(aud, false)
+		if !checkAudience {
+			return nil, fmt.Errorf(("invalid aud"))
+		}
+		// verify iss claim
+		iss := "fusionauth.io"
+		checkIss := token.Claims.(jwt.MapClaims).VerifyIssuer(iss, false)
+		if !checkIss {
+			return nil, fmt.Errorf(("invalid iss"))
+		}
+
+		return token, nil
+
+	})
+
+	fmt.Println("")
+	fmt.Printf("%+v\n", decodedToken.Claims)
+}
