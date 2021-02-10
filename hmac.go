270f90ef7e46ee7738d8bac8106a7e4df97a6c3e
Dan Moore <github@mooreds.com>
2021-02-10 11:31:31 -0700

    Added check for signing method.

--- hmac.go
+++ hmac.go
@@ -34,6 +34,9 @@ func main() {
 
 	// todo api
 	decodedToken, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+        		return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+     		}
 		return mySigningKey, nil
    	});
 
