1e9e1ac0d1094a34e13312c87c4a6208f59fd9cf
Dan Moore <github@mooreds.com>
2021-02-09 19:09:16 -0700

    Added rsa jwt signing.

---
+++ rsa.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"time"
+	"crypto/rsa"
+	"crypto/rand"
+)
+
+func main() {
+
+	privateKey, _ := rsa.GenerateKey(rand.Reader, 1024)
+        publicKey := privateKey.PublicKey
+
+	// user api
+	token := jwt.New(jwt.SigningMethodRS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["aud"] = "238d4793-70de-4183-9707-48ed8ecd19d9"
+	claims["sub"] = "19016b73-3ffa-4b26-80d8-aa9287738677"
+	claims["iss"] = "fusionauth.io"
+	claims["exp"] = time.Now().Add(time.Minute * 5).Unix()
+	claims["name"] = "Dan Moore"
+	var roles [1]string
+	roles[0] = "RETRIEVE_TODOS"
+	claims["roles"] = roles
+
+	tokenString, err := token.SignedString(privateKey)
+
+	if err != nil {
+		fmt.Println(fmt.Errorf("Something Went Wrong: %s", err.Error()))
+	}
+
+	fmt.Println(string(tokenString))
+
+	// todo api
+	decodedToken, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return &publicKey, nil
+   	});
+
+	fmt.Println("")
+        if err != nil {
+		fmt.Printf("Something Went Wrong: %s", err.Error())
+	} else {
+	  	fmt.Printf("%+v\n", decodedToken.Claims)
+	}
+}
